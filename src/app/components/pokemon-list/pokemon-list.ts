40429ca881600358b41a9072bbf9610623de1d51
Antoine <204738276+anT01ne42@users.noreply.github.com>
2025-06-19 12:00:24 +0200

    Add type filtering and update Pokémon list UI
    - Introduce type filtering using reactive form controls in `PokemonList` component.
    - Update Pokémon list UI to display types for each Pokémon.
    - Modify HTML structure to include filter options.
    - Add new favicon for the application.

--- src/app/components/pokemon-list/pokemon-list.ts
+++ src/app/components/pokemon-list/pokemon-list.ts
@@ -1,14 +1,16 @@
 import { Component, inject } from '@angular/core';
 import { PokemonServices } from '../../pokemon-services';
-import { Observable } from 'rxjs';
+import {combineLatest, map, Observable, startWith} from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-pokemon-list',
   imports: [
     AsyncPipe,
-    RouterLink
+    RouterLink,
+    ReactiveFormsModule
   ],
   templateUrl: './pokemon-list.html',
   styleUrl: './pokemon-list.css'
@@ -17,4 +19,39 @@ export class PokemonList {
   public pokemonService = inject(PokemonServices);
   pokemons$ = this.pokemonService.getPokemonsSummaryWithTypes();
   pokemonTypes$ = this.pokemonService.getAllPokemonTypes();
+
+  selectedTypeControl = new FormControl('all');
+
+  selectedOrderControl = new FormControl('id');
+
+  // Filtrage des pokemons
+  filteredPokemons$ = combineLatest([
+    this.pokemons$,
+    this.selectedTypeControl.valueChanges.pipe(startWith('all')),
+  ]).pipe(
+    map(([pokemons, selectedType]) => {
+      if (selectedType === 'all') {
+        return pokemons;
+      }
+      return pokemons.filter((pokemon) => {
+        return pokemon.types.some(t => t.type.name === selectedType);
+      });
+    })
+  );
+
+  /*filteredPokemons$ = combineLatest([
+    this.pokemons$,
+    this.selectedTypeControl.valueChanges.pipe(startWith('all')),
+
+  ]).pipe(
+    map(([pokemons, selectedType]) => {
+      if (selectedType === 'all') {
+        return pokemons;
+      }
+      return pokemons.filter((pokemon) => {
+        return pokemon.types.some(t => t.type.name === selectedType);
+      });
+    })
+  );*/
+
 }
